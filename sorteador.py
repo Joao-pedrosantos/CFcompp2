843c9d57bc2a1f45dad66a5c116ab6396016e51a
João Pedro Santos <joaoprs3@al.insper.edu.br>
2023-02-16 14:13:04 -0300

    sorteador

---
+++ sorteador.py
@@ -0,0 +1,9 @@
+from random import *
+comandos = ['00 00 00 00', '00 00 AA 00', 'AA 00 00', '00 AA 00', '00 00 AA', '00 AA', 'AA 00', '00', 'FF']
+enviar = []
+def sortear():
+    qtd = randint(10, 31)
+    for i in range(qtd):
+        enviar.append(choice(comandos))
+    return enviar
+print(sortear())
