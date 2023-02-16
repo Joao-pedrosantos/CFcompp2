9d6d875bd5ba4eb707593ce323570ad3806c1390
João Pedro Santos <joaoprs3@al.insper.edu.br>
2023-02-16 14:40:45 -0300

    com nome legal

--- sorteador.py
+++ sorteador.py
@@ -1,10 +1,12 @@
 from random import *
 comandos = ['00 00 00 00', '00 00 AA 00', 'AA 00 00', '00 AA 00', '00 00 AA', '00 AA', 'AA 00', '00', 'FF']
-enviar = []
+txbuffer = []
 def sortear():
     qtd = randint(10, 31)
     for i in range(qtd):
-        enviar.append(choice(comandos))
-        print(f'Comando {i+1}: {enviar[i]}')
-    return enviar
+        txbuffer.append(choice(comandos))
+        print(f'Comando {i+1}: {txbuffer[i]}')
+    return txbuffer
+
+
 print(sortear())
