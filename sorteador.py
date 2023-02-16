ccbe085c80b21bbf354f8c86a0c40a2f8bce9f80
João Pedro Santos <joaoprs3@al.insper.edu.br>
2023-02-16 14:52:10 -0300

    sorteia em byte

--- sorteador.py
+++ sorteador.py
@@ -1,11 +1,13 @@
 from random import *
 comandos = ['00 00 00 00', '00 00 AA 00', 'AA 00 00', '00 AA 00', '00 00 AA', '00 AA', 'AA 00', '00', 'FF']
 txbuffer = []
+bytearray = [bytes.fromhex(i) for i in comandos]
+
 def sortear():
     qtd = randint(10, 31)
     for i in range(qtd):
-        txbuffer.append(choice(comandos))
-        print(f'Comando {i+1}: {txbuffer[i]}')
+        txbuffer.append(choice(bytearray))
+        print(f'Comando {i+1}: {txbuffer[i]} ou {txbuffer[i].hex()}')
     return txbuffer
 
 
